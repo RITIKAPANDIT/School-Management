468cad7fe0c9630de0ba9e75dfbb3544c7e6f4e7
RITIKAPANDIT <120336021+RITIKAPANDIT@users.noreply.github.com>
2024-08-24 14:51:34 +0530

    Educase

--- db.js
+++ db.js
@@ -1,10 +1,22 @@
-const mysql=require('mysql2/promise')
-const db=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'ritika',
-    database:'student_db'
-})
+// const mysql=require('mysql2/promise')
+// const db=mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     password:'ritika',
+//     database:'student_db'
+// })
 
 
-     module.exports=db
+//      module.exports=db
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const schoolRoutes = require('./controllers/student.controller');
+
+// // Middleware
+// app.use(bodyParser.json());
+
+// // Routes
+// app.use('/api/schools', schoolRoutes);
+
+// // Server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -15,5 +34,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
