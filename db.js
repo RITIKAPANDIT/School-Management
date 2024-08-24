672cfd2643f765000648a72194d3f2e10e156e38
RITIKAPANDIT <120336021+RITIKAPANDIT@users.noreply.github.com>
2024-08-24 17:10:23 +0530

    Educase

--- db.js
+++ db.js
@@ -10,40 +10,40 @@
 //      module.exports=db
 
 
-// const mysql = require('mysql2/promise');
-// require('dotenv').config();
-
-// const db = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT,
-// });
-
-// module.exports = db;
-
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-const url = require('url');
-
-const dbUrl = process.env.DATABASE_URL;
-console.log('Database URL:', dbUrl);  // Check if it's properly loaded
-
-if (!dbUrl) {
-    throw new Error('DATABASE_URL is not defined in .env file');
-}
-
-const parsedUrl = url.parse(dbUrl);
-const [username, password] = parsedUrl.auth.split(':');
 
 const db = mysql.createPool({
-    host: parsedUrl.hostname,
-    user: username || '',
-    password: password || '',
-    database: parsedUrl.pathname.split('/')[1],
-    port: parsedUrl.port || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
 module.exports = db;
+
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+// const url = require('url');
+
+// const dbUrl = process.env.DATABASE_URL;
+// console.log('Database URL:', dbUrl);  // Check if it's properly loaded
+
+// if (!dbUrl) {
+//     throw new Error('DATABASE_URL is not defined in .env file');
+// }
+
+// const parsedUrl = url.parse(dbUrl);
+// const [username, password] = parsedUrl.auth.split(':');
+
+// const db = mysql.createPool({
+//     host: parsedUrl.hostname,
+//     user: username || '',
+//     password: password || '',
+//     database: parsedUrl.pathname.split('/')[1],
+//     port: parsedUrl.port || 3306,
+// });
+
+// module.exports = db;
 
